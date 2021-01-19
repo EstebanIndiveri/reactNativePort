611887753e37ec9403c4e29b6000b6b9e9496dd3
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-01-18 22:12:31 -0300

    ready to start

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { Text,StyleSheet,View} from 'react-native';
+import React,{useState} from 'react';
+import { Text,StyleSheet,View,FlatList} from 'react-native';
 
 
 const App = () => {
+  const [citas, setCitas] = useState([
+    {id:"1", paciente:"hook",propietario:"juan",sintomas:"no come"},
+    {id:"2", paciente:"redux",propietario:"itzel",sintomas:"no duerme"},
+    {id:"3", paciente:"native",propietario:"jose",sintomas:"no ladra"},
+  ]);
   console.log('desde consola');
   return (
     <>
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de citas</Text>
+      <FlatList
+        data={citas}
+        renderItem={({item})=>(
+          <View>
+            <Text>{item.paciente}</Text>
+          </View>
+        )}
+        keyExtractor={cita=>cita.id}
+      />
+      {/* {citas.map(cita=>(
+        <View key={cita.id}>
+          <Text>{cita.paciente}</Text>
+        </View>
+      ))} */}
     </View>
 
     </>
